579459c063a717412c7d7e55bfffaf120edc503f
msdf2244 <64487939+msdf2244@users.noreply.github.com>
2020-10-31 15:30:04 -0500

    boundaries (I love potatoes 'dinich')

--- py_game.py
+++ py_game.py
@@ -1,5 +1,5 @@
 import pygame
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 # this is necessary to start up all the pygame modules:
 pygame.init()
@@ -30,6 +30,8 @@ global playerX, playerY
 playerX = 370
 playerY = 480
 
+playerX_change = 0 
+
 
 # player function that takes x and y coordinates
 def player(x, y):
@@ -43,36 +45,40 @@ left_button = 13
 right_button = 15
 
 
-class GPIO_Handler:
-    def GPIO_Setup(self):
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(left_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(right_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# """ class GPIO_Handler:
+#     def GPIO_Setup(self):
+#         GPIO.setwarnings(False)
+#         GPIO.setmode(GPIO.BOARD)
+#         GPIO.setup(left_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#         GPIO.setup(right_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    def left_callback(channel):
-        global playerX
-        print("Left button pressed")
-        playerX -= 50
+#     def left_callback(channel):
+#         global playerX
+#         print("Left button pressed")
+#         playerX -= 50
 
-    def right_callback(channel):
-        global playerX
-        print("Right button pressed")
-        playerX += 500
+#     def right_callback(channel):
+#         global playerX
+#         print("Right button pressed")
+#         playerX += 500 """
 
 
-GPIO_Handler.GPIO_Setup()
+# GPIO_Handler.GPIO_Setup()
 
-GPIO.add_event_detect(left_button, GPIO.RISING,
-                      callback=GPIO_Handler.left_callback, bouncetime=50)
-GPIO.add_event_detect(right_button, GPIO.RISING,
-                      callback=GPIO_Handler.right_callback, bouncetime=50)
+# GPIO.add_event_detect(left_button, GPIO.RISING, callback=GPIO_Handler.left_callback, bouncetime=50)
+# GPIO.add_event_detect(right_button, GPIO.RISING, callback=GPIO_Handler.right_callback, bouncetime=50)
 
 
 # The game loop. Where all the logic of the game is. Start with a 'crashed' boolean that determines when the game loop ends.
 running = True
 
 while running:
+
+    # RGB control for bacground. Takes tuple as argument
+    screen.fill((0, 0, 0))
+    #Set background image 
+    screen.blit(background,(0,0))
+    
 
     # pygame module helps with "event handling." (AKA clicking, mouse placement, keys pressed)
     for event in pygame.event.get():  # gets list of events
@@ -82,20 +88,15 @@ while running:
 
         # if keystroke is pressed check whether its right or left. Will change later when we get to it. (Find some way of configuring buttons)
         if event.type == pygame.KEYDOWN:  # KEYDOWN detects keystoke event
-            if event.key == pygame.K_LEFT:
-                print("Left key pressed")
+            if event.key == pygame.K_LEFT and playerX > 50:
                 playerX -= 50
-            if event.key == pygame.K_RIGHT:
-                print("Right key pressed")
+            if event.key == pygame.K_RIGHT and playerX < 700 :
                 playerX += 50
-        if event.type == pygame.KEYUP:  # Detects when key is released
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                print("Key has been released")
+    #     if event.type == pygame.KEYUP:  # Detects when key is released
+    #         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 
-    # RGB control for bacground. Takes tuple as argument
-    screen.fill((0, 0, 0))
-    #Set background image 
-    screen.blit(background,(0,0))
+    #             playerX_change = 0
+                
     # player function must come after the screen fill to avoid covering up image
     player(playerX, playerY)
     pygame.display.update()
